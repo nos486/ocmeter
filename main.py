afbda6f432441e478119650f765b3eca5a90d688
sina <nos486@gmail.com>
2023-01-29 22:56:13 +0330

    fix sort by download

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import pandas as pd
 UPDATE_DELAY = 10
 
 def get_size(bytes):
-    """
-    Returns size of bytes in a nice format
-    """
     for unit in ['', 'K', 'M', 'G', 'T', 'P']:
         if bytes < 1024:
             return f"{bytes:.2f}{unit}B"
@@ -20,7 +17,6 @@ io = psutil.net_io_counters(pernic=True)
 
 while True:
 
-    time.sleep(UPDATE_DELAY)
     userList = {}
     for userData in subprocess.check_output("who", shell=True).decode("utf-8").split("\n"):
         if userData != "":
@@ -36,16 +32,16 @@ while True:
             upload_speed, download_speed = io_2[iface].bytes_sent - iface_io.bytes_sent, io_2[iface].bytes_recv - iface_io.bytes_recv
             if iface in userList.keys():
                 ifaceName = userList[iface]
-                ifDownload = get_size(io_2[iface].bytes_sent)
-                ifUpload = get_size(io_2[iface].bytes_recv)
-                ifDownloadSpeed = f"{get_size(upload_speed / UPDATE_DELAY)}/s"
-                ifUploadSpeed = f"{get_size(download_speed / UPDATE_DELAY)}/s"
+                ifDownload = io_2[iface].bytes_sent
+                ifUpload = io_2[iface].bytes_recv
+                ifDownloadSpeed = upload_speed / UPDATE_DELAY
+                ifUploadSpeed = download_speed / UPDATE_DELAY
             else:
                 ifaceName = iface
-                ifDownload = get_size(io_2[iface].bytes_recv)
-                ifUpload = get_size(io_2[iface].bytes_sent)
-                ifDownloadSpeed = f"{get_size(download_speed / UPDATE_DELAY)}/s"
-                ifUploadSpeed = f"{get_size(upload_speed / UPDATE_DELAY)}/s"
+                ifDownload = io_2[iface].bytes_recv
+                ifUpload = io_2[iface].bytes_sent
+                ifDownloadSpeed = download_speed / UPDATE_DELAY
+                ifUploadSpeed = upload_speed / UPDATE_DELAY
             data.append({
                 "iface": ifaceName,
                 "Download": ifDownload,
@@ -56,6 +52,12 @@ while True:
 
     io = io_2
     df = pd.DataFrame(data)
-    df.sort_values("Download Speed", inplace=True, ascending=False)
+    df.sort_values("Download", inplace=True, ascending=False)
+    df["Download"] = df["Download"].apply(lambda x: get_size(x))
+    df["Upload"] = df["Upload"].apply(lambda x: get_size(x))
+    df["Download Speed"] = df["Download Speed"].apply(lambda x: get_size(x))
+    df["Upload Speed"] = df["Upload Speed"].apply(lambda x: get_size(x))
+
     os.system("clear")
     print(df.to_string())
+    time.sleep(UPDATE_DELAY)
